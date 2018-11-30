be9939dc5f9949c80eabecf83008bcb7cd0dcf19
Brandon Best <bbest@cs.ualberta.ca>
2018-11-30 13:41:31 -0700

    Ex 4 done, working on 5

--- ex4.m
+++ ex4.m
@@ -19,8 +19,78 @@ y4 = funcVals(f4);
 y5 = funcVals(f5);
 
 
-plot(0:5,b,'*-',0:5,y0,'-o',0:5,y1,'--o',0:5,y2,'-o',0:5,y3,'--o',0:5,y4,'-o',0:5,y5,'--o');
-legend('f0', 'f1','f2', 'f3', 'f4', 'f5');
+% plot(0:5,b,'*-',0:5,y0,'-o',0:5,y1,'--o',0:5,y2,'-o',0:5,y3,'--o',0:5,y4,'-o',0:5,y5,'--o');
+% legend('f0', 'f1','f2', 'f3', 'f4', 'f5');
+
+syms x
+
+
+func_0 = @(x) 5.5833;  
+func_1 = @(x) 1.3190 + 1.7057*x;
+func_2 = @(x) 1.0036 + 2.1789*x - 0.0946*x^2;
+func_3 = @(x) 0.7897 + 3.1557*x - 0.6294*x^(2) + 0.0713*x^3;
+func_4 = @(x) 0.9718 + 0.12*x + 2.6340*x^(2) - 0.9912*x^(3) + 0.1063*x^4;
+func_5 = @(x) 1 - 3.1783*x + 8.3042*x^(2) - 4.2125*x^(3) + 0.8458*x^(4) - 0.0592*x^(5);
+
+% f5_vals = eval(func_5,x,);
+% 
+% plot(linspace(0,0.01,5),f5_vals);
+
+f0_prime = slopes(func_0, x);
+f1_prime = slopes(func_1, x);
+f2_prime = slopes(func_2, x);
+f3_prime = slopes(func_3, x);
+f4_prime = slopes(func_4, x);
+f5_prime = slopes(func_5, x);
+h = 0.0001;
+
+F = forwardDiff(func_2,x,h);
+B = backwardDiff(func_2,x,h);
+C = centralDiff(func_2,x,h);
+
+f = f2_prime;
+
+plot(0:5,f,'*-',0:5,F,'o-',0:5,B,'o--',0:5,C,'*--');
+legend('f prime','forward diff', 'backward diff','central diff');
+
+[f, F, B, C]
+
+function fwd = forwardDiff(func, x, h)
+fwd = [];
+for i=0:5
+   f_i = eval(subs(func,x,i+h))-eval(subs(func,x,i));
+   f_i = f_i/h;
+   fwd = [fwd; f_i];
+end
+end
+
+function bck = backwardDiff(func, x, h)
+bck = [];
+for i=0:5
+   f_i = eval(subs(func,x,i))-eval(subs(func,x,i-h));
+   f_i = f_i/h;
+   bck = [bck; f_i];
+end
+end
+
+function cent = centralDiff(func, x, h)
+cent = [];
+for i=0:5
+   f_i = eval(subs(func,x,i+h))-eval(subs(func,x,i-h));
+   f_i = f_i/(2*h);
+   cent = [cent; f_i];
+end
+end
+
+
+function y = slopes(func, x)
+y = [];
+for i=0:5
+    y_i = eval( (subs(diff(func,x,1),x,i)) );
+    y = [y; y_i];
+end
+end
+
 function y_i = polynomialVals(x,i)
     s = size(x);
     y_i = 0;
@@ -42,20 +112,12 @@ end
 end
 
 function t = plotTangent()
-x = linspace(-2, 2, 500);
-y = x .^ 2 + 2;
-plot(x, y, 'b-', 'LineWidth', 2);
-grid on;
-fontSize = 20;
-xlabel('X', 'FontSize', fontSize);
-ylabel('Y', 'FontSize', fontSize);
-ylim([0, 7]);
 % Pick a point at -1 (or wherever) and plot the tangent
-xTangent = -1;
+xTangent = 3;
 % Equation of the tangent is y = slope*x + offset
 % From the equation y=x^2 we know the slope is 2*x.
 % Find the slope at x = xTangent.
-slope = 2 * xTangent;
+slope = 3* xTangent;
 % We want to plot the tangent line where it just touches the curve,
 % so we need to know the y value at xTangent.
 yTangent = xTangent .^ 2 + 2;  % Y value of curve at x = xTangent.
--- ex5.m
+++ ex5.m
@@ -1,1 +1,39 @@
 %% Excercise 5
+% distance travelled
+dist = [0 1.25 2.5 3.75 5 6.25 7.5 8.75 10];
+% fuel efficiency at that moment
+y = [0.26 0.208 0.172 0.145 0.126 0.113 0.104 0.097 0.092]; 
+
+A = fliplr(vander(dist));
+
+f = A\y';
+
+a = 0;
+b = 10;
+
+syms x
+
+func = @(x) 0.26 - 0.0555.*x + 0.0174.*x.^2 - 0.0072.*x.^3 + 0.0021.*x.^4 - 0.0004.*x.^5;
+
+I = integral(func,a,b);
+
+M = midpointRule(a,b,func,x);
+T = trapezoidRule(a,b,func,x);
+S = simpsonRule(a,b,func,x);
+
+[I M T S]
+
+function M = midpointRule(a,b,func,x)
+    f = eval(subs(func,x,((a+b)/2)));
+    M = (b-a)*f;
+end
+
+function T = trapezoidRule(a,b,func,x)
+    f = eval(subs(func,x,a)) + eval(subs(func,x,b));
+    T = ((b-a)/2)*f;
+end
+
+function S = simpsonRule(a,b,func,x)
+f = eval(subs(func,x,a)) + 4*eval(subs(func,x,((a+b)/2))) + eval(subs(func,x,b));
+S = ((b-a)/6) * f;
+end
